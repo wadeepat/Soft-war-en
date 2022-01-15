e816cfd86edcb760aeda247570ce542ab56de133
vVviewVv <viewissara1246@gmail.com>
2022-01-15 18:08:00 +0700

    unload + save in database

--- frontend/src/components/upload.js
+++ frontend/src/components/upload.js
@@ -1,37 +1,62 @@
-import React, { Component } from "react";
+
+import React,{useState } from 'react';
 import axios from 'axios';
 
-class Upload extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: null
-    };
+function Upload() {
+  const [userInfo, setuserInfo] = useState({
+    file:[],
+    filepreview:null,
+   });
 
-    this.onImageChange = this.onImageChange.bind(this);
+  const handleInputChange = (event) => {
+    setuserInfo({
+      ...userInfo,
+      file:event.target.files[0],
+      filepreview:URL.createObjectURL(event.target.files[0]),
+    });
+
   }
 
-  onImageChange = event => {
-    if (event.target.files && event.target.files[0]) {
-      let img = event.target.files[0];
-      this.setState({
-        image: URL.createObjectURL(img)
-      });
-    }
-  };
+  const [isSucces, setSuccess] = useState(null);
 
-  render() {
-    return (
-      <div>
-        <div className="up-down-edit">
-          <div className="upload-pic-area">
-            <img src={this.state.image} />
-            <input type="file" name="myImage" onChange={this.onImageChange} />
-            <a href="/edit"><button type="button"  id="submit">Upload image</button></a>
-          </div>
+  const submit = async () =>{
+    const formdata = new FormData(); 
+    formdata.append('avatar', userInfo.file);
+
+    axios.post("http://localhost:8080/imageupload", formdata,{   
+            headers: { "Content-Type": "multipart/form-data" } 
+    })
+    .then(res => { // then print response status
+      console.warn(res);
+      if(res.data.success === 1){
+        setSuccess("Image upload successfully");
+      }
+
+    })
+  }
+
+  return (
+    <div className="container mr-60">
+
+      <div className="up-down-edit" align = 'center'>
+      {isSucces !== null ? <h4> {isSucces} </h4> :null }
+        <div className="form-row" align = 'center'>
+          <label >Select Image :</label>
+          <input type="file" className="form-control"  name="myImage"  onChange={handleInputChange} />
+        </div>
+        
+      {userInfo.filepreview !== null ? 
+        <img className="previewimg"  src={userInfo.filepreview} alt="UploadImage"  />
+      : null}
+
+        <div className="upload-pic-area">
+          <a href="/edit">
+            <button type="submit" className="btn btn-dark"onClick={()=>submit()} > Save </button>
+          </a>
+          
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Upload;
