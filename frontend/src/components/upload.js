a849e454bbcbf9f2deb52d113148864951641a2e
vVviewVv <viewissara1246@gmail.com>
2022-01-15 14:45:17 +0700

    upload(only display image)

--- frontend/src/components/upload.js
+++ frontend/src/components/upload.js
@@ -1,14 +1,37 @@
-import react from 'react';
+import React, { Component } from "react";
+import axios from 'axios';
 
-const Upload = () => {
-return(
-    <div className="up-down-edit" >
-        <div className="upload-pic-area">
-        <button type="button" className="btn-upload">Upload image</button>
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null
+    };
+
+    this.onImageChange = this.onImageChange.bind(this);
+  }
+
+  onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      this.setState({
+        image: URL.createObjectURL(img)
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="up-down-edit">
+          <div className="upload-pic-area">
+            <img src={this.state.image} />
+            <input type="file" name="myImage" onChange={this.onImageChange} />
+            <a href="/edit"><button type="button"  id="submit">Upload image</button></a>
+          </div>
         </div>
-    </div>
-)
-
+      </div>
+    );
+  }
 }
-
 export default Upload;
